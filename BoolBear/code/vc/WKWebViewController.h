8c0f7013afbe2f92abeb18dcbf645965e2c31e4a
liu ru qiang <liuruqiang@xueleyun.com>
2020-06-11 16:57:33 +0800

    优化

---
+++ BoolBear/code/vc/WKWebViewController.h
@@ -0,0 +1,18 @@
+//
+//  WKWebViewController.h
+//  TestWKWebView
+//
+//  Created by lk on 2019/5/13.
+//  Copyright © 2019 lk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "CustomNavigationViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface WKWebViewController : CustomNavigationViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
